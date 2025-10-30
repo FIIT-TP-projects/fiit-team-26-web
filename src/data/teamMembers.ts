096163268c91f61f33c42e7764687fdc7b05be4b
Heorhi Davydau <heorhid@gmail.com>
2025-10-30 18:20:01 +0100

    [fix] updated team member descriptions and photo links

--- src/data/teamMembers.ts
+++ src/data/teamMembers.ts
@@ -4,7 +4,8 @@ export const teamData = {
     role: "Vedúci projektu",
     photo:
       "https://media.licdn.com/dms/image/v2/C5603AQHjiqQ8_bn5ag/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1517065607184?e=1762992000&v=beta&t=e-gF99Uy9180FODpxW7pVfhxSr0yolpI6CJmijR-w90",
-    description: "Som vysokoškolský učiteľ a výskumník na FIIT STU, ktorý sa venuje umelej inteligencii, robotike a spracovaniu právnych textov. Zároveň mám vášeň pre turistiku a rád spájam vedu s tvorivými aj osobnými projektmi.\n",
+    description:
+      "Som vysokoškolský učiteľ a výskumník na FIIT STU, ktorý sa venuje umelej inteligencii, robotike a spracovaniu právnych textov. Zároveň mám vášeň pre turistiku a rád spájam vedu s tvorivými aj osobnými projektmi.\n",
     linkedin: "https://www.linkedin.com/in/martin-kom%C3%A1k-742789128/",
   },
   members: [
@@ -15,7 +16,7 @@ export const teamData = {
       photo:
         "https://media.licdn.com/dms/image/v2/D4E03AQED5iXBJBJNmw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1697709962929?e=1762992000&v=beta&t=LZ0SIPxOzqRDjOW7Z4f-mwAsUwSsR61vSO41P9thuLA",
       description:
-        "Tuto stranku robil ja, a cely frontend z toho. A taktiez som sa postaral o vsetky dalsie stranky.",
+        "Zodpovedal som za celý frontend aplikácie aj webovej stránky. Navrhol som používateľské rozhranie, implementoval komponenty a zabezpečil plynulú interakciu medzi používateľom a systémom. Pracujem s technológiami ako React, Vue a TypeScript, pričom kladiem dôraz na prehľadný kód a používateľskú skúsenosť.",
       linkedin: "https://www.linkedin.com/in/heorhi-davydau/",
     },
     {
@@ -23,7 +24,7 @@ export const teamData = {
       name: "Bc. Adam Candrák",
       role: "Team leader",
       photo:
-        "https://media.licdn.com/dms/image/v2/D4D03AQEOqHw5BgjesA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1714640380562?e=1762992000&v=beta&t=HxeYtZXR3rUlkUcngHlmfpwEdKLD11jX5cOdrUpMqDY",
+        "https://media.licdn.com/dms/image/v2/D4D03AQGZqAc9ryiWRQ/profile-displayphoto-scale_400_400/B4DZovgK2gJEAo-/0/1761733554135?e=1763596800&v=beta&t=CXdlXwzi6f1qjN0MUQHk5KOmMaK2GtCN4NySxeJiYdo",
       description:
         "Zameriavam sa na agílne riadenie projektu prostredníctvom SCRUM metodiky s dôrazom na transparentnú komunikáciu. Venujem sa NLP technológií a súčasťou diplomovej práce riešim spracovanie dokumentov za účelom dodržania advokátskej mlčanlivosti.\n",
       linkedin: "https://www.linkedin.com/in/adam-candr%C3%A1k-78672a307/",
