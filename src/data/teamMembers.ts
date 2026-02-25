87e9668e21ac0e001b9fc4e12d1ed82bceb02f05
Heorhi Davydau <heorhid@gmail.com>
2026-02-25 14:21:01 +0100

    [fix] removed pravnik.jpg and updated image paths for team members in teamMembers.ts

--- src/data/teamMembers.ts
+++ src/data/teamMembers.ts
@@ -2,8 +2,7 @@ export const teamData = {
   leader: {
     name: "Ing. Martin Komák, PhD.",
     role: "Vedúci projektu",
-    photo:
-      "https://media.licdn.com/dms/image/v2/C5603AQHjiqQ8_bn5ag/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1517065607184?e=1762992000&v=beta&t=e-gF99Uy9180FODpxW7pVfhxSr0yolpI6CJmijR-w90",
+    photo: "./martinkomak.jpeg",
     description:
       "Som vysokoškolský učiteľ a výskumník na FIIT STU, ktorý sa venuje umelej inteligencii, robotike a spracovaniu právnych textov. Zároveň mám vášeň pre turistiku a rád spájam vedu s tvorivými aj osobnými projektmi.\n",
     linkedin: "https://www.linkedin.com/in/martin-kom%C3%A1k-742789128/",
@@ -13,8 +12,7 @@ export const teamData = {
       id: 1,
       name: "Bc. Heorhi Davydau",
       role: "Frontend vývojár",
-      photo:
-        "https://media.licdn.com/dms/image/v2/D4E03AQED5iXBJBJNmw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1697709962929?e=1762992000&v=beta&t=LZ0SIPxOzqRDjOW7Z4f-mwAsUwSsR61vSO41P9thuLA",
+      photo: "./heorhidavydau.jpeg",
       description:
         "Zodpovedal som za celý frontend aplikácie aj webovej stránky. Navrhol som používateľské rozhranie, implementoval komponenty a zabezpečil plynulú interakciu medzi používateľom a systémom. Pracujem s technológiami ako React, Vue a TypeScript, pričom kladiem dôraz na prehľadný kód a používateľskú skúsenosť.",
       linkedin: "https://www.linkedin.com/in/heorhi-davydau/",
@@ -23,8 +21,7 @@ export const teamData = {
       id: 2,
       name: "Bc. Adam Candrák",
       role: "Team leader",
-      photo:
-        "https://media.licdn.com/dms/image/v2/D4D03AQGZqAc9ryiWRQ/profile-displayphoto-scale_400_400/B4DZovgK2gJEAo-/0/1761733554135?e=1763596800&v=beta&t=CXdlXwzi6f1qjN0MUQHk5KOmMaK2GtCN4NySxeJiYdo",
+      photo: "./adamcandrak.jpeg",
       description:
         "Zameriavam sa na agílne riadenie projektu prostredníctvom SCRUM metodiky s dôrazom na transparentnú komunikáciu. Venujem sa NLP technológií a súčasťou diplomovej práce riešim spracovanie dokumentov za účelom dodržania advokátskej mlčanlivosti.\n",
       linkedin: "https://www.linkedin.com/in/adam-candr%C3%A1k-78672a307/",
@@ -33,8 +30,7 @@ export const teamData = {
       id: 3,
       name: "Bc. Vladyslav Gusiev",
       role: "Backend vývojár",
-      photo:
-        "https://media.licdn.com/dms/image/v2/D4D03AQGCQ92wZjeLqw/profile-displayphoto-shrink_400_400/B4DZTRJTgzHkAg-/0/1738675665363?e=1762992000&v=beta&t=8xUzmpNMmzTDzxYN0P7zmucOyQHSZA2omN0GhcHYHao",
+      photo: "./vladyslavgusiev.jpeg",
       description:
         "Nas najlepsi backend developer, ktory nam pomohl s projektom a pomohol nam s tim ako to ide.",
       linkedin: "https://www.linkedin.com/in/vladyslav-gusiev-9a736834b/",
@@ -43,8 +39,7 @@ export const teamData = {
       id: 4,
       name: "Bc. Jozef Židuliak",
       role: "AI/ML špecialista",
-      photo:
-        "https://media.licdn.com/dms/image/v2/D4E03AQGo9w24Ni6qLw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1718332689705?e=1762992000&v=beta&t=LAIq8E9zngWZumz5_MpSpAVjqA6lmS9uH6QQ170Vrdc",
+      photo: "./jozefziduliak.jpeg",
       description:
         "Zameriava sa na trénovanie AI modelov pre generovanie právnych dokumentov a optimalizáciu promptov.",
       linkedin: "https://www.linkedin.com/in/jozef-%C5%BEiduliak-069663271/",
@@ -53,8 +48,7 @@ export const teamData = {
       id: 5,
       name: "Bc. Samuel Csető",
       role: "Frontend vývojár",
-      photo:
-        "https://media.licdn.com/dms/image/v2/C5603AQFW0U8_IcYF_w/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1659108008444?e=1762992000&v=beta&t=OL-qzn-KZU7Ueq95G7pe0K8k-o10SD8PnCf6yXsXtdU",
+      photo: "./samuelcseto.jpeg",
       description:
         "Nas najlepsi frontend developer, ktory nam pomohl s projektom a pomohol nam s tim ako to ide.",
       linkedin: "https://www.linkedin.com/in/samuel-cset%C5%91-330706232/",
@@ -72,8 +66,7 @@ export const teamData = {
       id: 7,
       name: "Bc. Peter Brandajský",
       role: "AI/ML špecialista",
-      photo:
-        "https://media.licdn.com/dms/image/v2/D4E03AQE7BnVt9fynuw/profile-displayphoto-scale_400_400/B4EZd64dFYHcAg-/0/1750113313386?e=1762992000&v=beta&t=ejqaq9Oi03gnMH6ZzeFgsbjYis6fl1-JBxlq5c5BLDI",
+      photo: "./peterbrandajsky.jpeg",
       description:
         "Nas najlepsi AI/ML specialist, ktory nam pomohl s projektom a pomohol nam s tim ako to ide.",
       linkedin: "https://www.linkedin.com/in/peter-brandajsk%C3%BD-436340208/",
